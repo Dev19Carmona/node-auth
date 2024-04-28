efa082110b7bb65307d719e7b950c303d8926235
Camilo Carmona Ramirez <camilocr3@hotmail.com>
2024-04-28 16:27:14 -0500

    Lo que sea

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -6,10 +6,6 @@ import { AppointmentRoutes } from "./appointment/routes";
 export class AppRoutes {
   static get routes(): Router {
     const router = Router()
-    router.use('/', (req, res, next) => { 
-      res.json({ ServerOn: true, routes: ['/api/auth', '/api/pet', '/api/appointment'] }) 
-      next()
-    })
     router.use('/api/auth', AuthRoutes.routes)
     router.use('/api/pet', PetRoutes.routes)
     router.use('/api/appointment', AppointmentRoutes.routes)
