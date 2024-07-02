fcb36089b7f9d807be237b290516992133228e23
Camilo Carmona Ramirez <camilocr3@hotmail.com>
2024-07-01 19:11:36 -0500

    ConstantsMongo

---
+++ src/infrastructure/datasources/mongo/constants.ts
@@ -0,0 +1,7 @@
+import { UpsertObject } from "./interfaces";
+
+export const upsertObject:UpsertObject = {
+    upsert: true,
+    new: true,
+    setDefaultsOnInsert: true
+}
---
+++ src/infrastructure/datasources/mongo/interfaces.ts
@@ -0,0 +1,5 @@
+export interface UpsertObject {
+    upsert: boolean,
+    new: boolean,
+    setDefaultsOnInsert: boolean
+}
